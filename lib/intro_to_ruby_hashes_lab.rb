3127cb0cb098c913e4710a4558401bc7bd2e512e
plony benav <menachemfuterfas@gmail.com>
2020-11-12 18:11:26 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -4,8 +4,10 @@ def my_hash_creator(key, value)
 end
 
 def read_from_hash(hash, key)
-  # return the correct value using the hash and key parameters
+  
 end
+
+
 
 def update_counting_hash(hash, key)
   # given a hash an a key as parameters, return an updated hash
